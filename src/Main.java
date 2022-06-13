704698fff68ca87032ac62b072eeaab3b78f119a
Софья Костева <diamondnote@yandex.ru>
2022-06-13 15:35:19 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 8530; //стоимость билета
+        int spend = 20; //рублей за милю
+
+        int miles = ticket / spend;
+
+        System.out.println("За купленный билет, вы получите миль(ю): " + miles);
+    }
+}
+
+
+
+
+
+
+
+
